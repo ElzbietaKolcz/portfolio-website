88649341ac079d08d2f4b5c2e2a5ce35713207f7
ek <elzbieta.kolcz.13@gmail.com>
2023-09-04 22:32:30 +0200

    Add two / to base in vite.config file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "portfolio-website",
+  base: "/portfolio-website/",
   preview: {
     host: true,
     port: 8080,
