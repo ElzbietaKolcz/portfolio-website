26cf287ae333d615fbf4fea238cf84262636425e
Elżbieta Kołcz <elzbieta.kolcz.13@gmail.com>
2026-03-23 12:37:25 +0100

    fix: remove unused SOCIAL_LINKS dead code and asset imports from Footer
    Signed-off-by: Elżbieta Kołcz <elzbieta.kolcz.13@gmail.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,4 @@
-import { behanceFooter, gitHubFooter, linkedInFooter } from "../assets";
 import { t } from "../i18n";
-
-const SOCIAL_LINKS = [
-  { id: "behance-link", href: "https://www.behance.net/elbietakocz/", icon: behanceFooter, key: "behance" },
-  { id: "github-link", href: "https://www.github.com/ElzbietaKolcz", icon: gitHubFooter, key: "github" },
-  { id: "linkedin-link", href: "https://www.linkedin.com/in/elzbieta-kolcz", icon: linkedInFooter, key: "linkedin" },
-];
 
 export default function Footer() {
   return (
@@ -14,7 +7,7 @@ export default function Footer() {
         {" "}
         {t("footer.copyright", { year: new Date().getFullYear() })}{" "}
       </p>
-     
+
     </footer>
   );
 }
